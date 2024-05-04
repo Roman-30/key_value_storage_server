d085003ef809ee4dbaadb1d5e1514d02c0eb5472
Roman-30 <Romses2016@yandex.ru>
2024-05-04 16:22:01 +0300

    init project

---
+++ src/main/java/cs/vsu/ru/keyvaluestorageserver/KeyValueStorageServerApplication.java
@@ -0,0 +1,13 @@
+package cs.vsu.ru.keyvaluestorageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyValueStorageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyValueStorageServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cs/vsu/ru/keyvaluestorageserver/KeyValueStorageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.vsu.ru.keyvaluestorageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyValueStorageServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
